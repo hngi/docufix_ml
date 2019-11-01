769b74fb9bb79a47ffd745e9ce64f91c21192d25
agbanusi <53221092+agbanusi@users.noreply.github.com>
2019-11-01 09:13:39 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -8,7 +8,7 @@ listen = ['high', 'default', 'low']
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
-result = q.enqueue(sim())
+result = q.enqueue(sim(c))
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(Queue, listen))
